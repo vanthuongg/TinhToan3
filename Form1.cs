2fd9528255c8b280c89b253071058cafddd45ca9
vanthuongg <thuongdo0981@gmail.com>
2025-09-10 13:19:57 +0700

    commit

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buoi07_TinhToan3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSo1.Text = txtSo2.Text = "0";
+            radCong.Checked = true;             //đầu tiên chọn phép cộng
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult dr;
+            dr = MessageBox.Show("Bạn có thực sự muốn thoát không?",
+                                 "Thông báo", MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+                this.Close();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            //lấy giá trị của 2 ô số
+            double so1, so2, kq = 0;
+            so1 = double.Parse(txtSo1.Text);
+            so2 = double.Parse(txtSo2.Text);
+            //Thực hiện phép tính dựa vào phép toán được chọn
+            if (radCong.Checked) kq = so1 + so2;
+            else if (radTru.Checked) kq = so1 - so2;
+            else if (radNhan.Checked) kq = so1 * so2;
+            else if (radChia.Checked && so2 != 0) kq = so1 / so2;
+            //Hiển thị kết quả lên trên ô kết quả
+            txtKq.Text = kq.ToString();
+        }
+    }
+}
